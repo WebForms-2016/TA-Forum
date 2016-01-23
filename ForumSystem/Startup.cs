79fdf4304337c616811878634434b2508172a515
Pavel <pawel.angelow@gmail.com>
2016-01-23 15:43:43 +0200

    Initial Web Forms structure

---
+++ ForumSystem/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForumSystem.Startup))]
+namespace ForumSystem
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
