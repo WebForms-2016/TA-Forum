859ced08446a2eb2cd4cde86a14aa9ecd3c5c937
Pavel <pawel.angelow@gmail.com>
2016-01-25 12:24:51 +0200

    Added users admin page and service for users

---
+++ Services/ForumSystem.Services.Data/UserService.cs
@@ -0,0 +1,22 @@
+﻿namespace ForumSystem.Services.Data
+{
+    using System.Linq;
+
+    using Models;
+    using ForumSystem.Services.Data.Contracts;
+    using ForumSystem.Data;
+    public class UserService : IUserService
+    {
+        private readonly IRepository<User> users;
+
+        public UserService(IRepository<User> usersRepo)
+        {
+            this.users = usersRepo;
+        }
+
+        public IQueryable<User> GetAll()
+        {
+            return this.users.All();
+        }
+    }
+}
